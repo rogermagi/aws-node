647810ca666972bf6188f8887d30bf671f48f75a
Roger Paulraj <roger_magi@yahoo.com>
2021-10-27 11:04:38 -0400

    changed Message in App.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) =>{
-    res.send("Success");
+    res.send("Fantastic - Application deployed to AWS");
 });
 
 const port = process.env.port || 3000;
